dc4ca45f861c7291b19bcab87d8c3692d8b761e6
s58122519013 <78157975+s58122519013@users.noreply.github.com>
2021-01-28 23:06:03 +0700

    Add files via upload

---
+++ Datas/Adata.cs
@@ -0,0 +1,33 @@
+﻿using PTSSRU.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PTSSRU.Datas
+{
+    public static class Adata
+    {
+        public static IList<Ismodel> AA { get; private set; }
+
+        static Adata()
+        {
+            AA = new List<Ismodel>();
+
+            AA.Add(new Ismodel
+            {
+                Name = "Baboon",
+                Location = "13.776692280120338,100.50902230906989",
+                Details = "Baboons are African and Arabian Old World monkeys belonging to the genus Papio, part of the subfamily Cercopithecinae.",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Papio_anubis_%28Serengeti%2C_2009%29.jpg/200px-Papio_anubis_%28Serengeti%2C_2009%29.jpg"
+            });
+
+            AA.Add(new Ismodel
+            {
+                Name = "Capuchin Monkey",
+                Location = "13.776692280120338,100.50902230906989",
+                Details = "The capuchin monkeys are New World monkeys of the subfamily Cebinae. Prior to 2011, the subfamily contained only a single genus, Cebus.",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Capuchin_Costa_Rica.jpg/200px-Capuchin_Costa_Rica.jpg"
+            });
+        }
+    }
+}
---
+++ Datas/Palace.cs
@@ -0,0 +1,88 @@
+﻿using PTSSRU.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PTSSRU.Datas
+{ 
+    public static class Palace
+{
+    public static IList<Ismodel> P { get; private set; }
+
+    static Palace()
+    {
+        P = new List<Ismodel>();
+
+        P.Add(new Ismodel
+        {
+            Name = "อาทรทิพยนิวาสน์",
+            Location = "Africa & Asia",
+            Details = "ooooooo",
+            ImageUrl = "https://sv1.picz.in.th/images/2021/01/11/lKpjqk.jpg",
+        });
+
+        P.Add(new Ismodel
+        {
+            Name = "อาทรทิพยนิวาสน์2",
+            Location = "Central & South America",
+            Details = "a",
+            ImageUrl = "https://sv1.picz.in.th/images/2021/01/11/lKpl8v.jpg"
+        });
+        
+         P.Add(new Ismodel
+        {
+            Name = "อาคารจุฑารัตนาภรณ์",
+            Location = "Central & South America",
+            Details = "a",
+            ImageUrl = "https://sv1.picz.in.th/images/2021/01/11/lKpD7E.jpg"
+         });
+            P.Add(new Ismodel
+            {
+                Name = "กำลังซ่อม",
+                Location = "Africa & Asia",
+                Details = "ooooooo",
+                ImageUrl = "https://sv1.picz.in.th/images/2021/01/11/lKpuln.jpg"
+            });
+
+            P.Add(new Ismodel
+            {
+                Name = "อาคารศศิพงษ์ประไพ",
+                Location = "Africa & Asia",
+                Details = "ooooooo",
+                ImageUrl = "https://sv1.picz.in.th/images/2021/01/11/lKpCtS.jpg"
+            });
+
+            P.Add(new Ismodel
+            {
+                Name = "ศาลปราสาทสมเด็จพระนางเจ้าสวนสุนันทากุมารีรัตน์",
+                Location = "Africa & Asia",
+                Details = "ooooooo",
+                ImageUrl = "https://sv1.picz.in.th/images/2021/01/11/lKp6Gg.jpg"
+            });
+
+            P.Add(new Ismodel
+            {
+                Name = "อาคารสายสุรธานภดล",
+                Location = "Africa & Asia",
+                Details = "ooooooo",
+                ImageUrl = "https://sv1.picz.in.th/images/2021/01/11/lKpP2V.jpg"
+            });
+
+            P.Add(new Ismodel
+            {
+                Name = "โบราณสถาน หลุมหลบภัย เนินพระนาง",
+                Location = "Africa & Asia",
+                Details = "ooooooo",
+                ImageUrl = "https://sv1.picz.in.th/images/2021/01/11/lKpAgN.jpg"
+            });
+
+            P.Add(new Ismodel
+            {
+                Name = "อนุสาวรีย์ พระองค์เจ้าสุนันทากุมารีรัตน์",
+                Location = "Africa & Asia",
+                Details = "ooooooo",
+                ImageUrl = "https://sv1.picz.in.th/images/2021/01/11/lKpsFQ.jpg"
+            });         
+        }
+}
+}
